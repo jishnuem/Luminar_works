0fb33f7cddf220b9d3f084e310703004f45c2dac
Jishnu <jishnu175@gmail.com>
2021-06-10 11:01:37 +0530

    js

---
+++ JavaScript/fund2.js
@@ -0,0 +1,15 @@
+//console.log(2+5)
+
+// let p_name="Jishnu";
+// let age=22;
+// console.log("hi iam " + p_name,age +" years old")
+
+let state="kerala";
+let active_casese=15000;
+let cure=20000;
+let death=423;
+let active=true;
+//console.log(`in ${state} ${active_casese} new cases and ${cure} death ${death} has been reported`);
+console.log(typeof state);
+console.log(typeof cure);
+console.log(typeof active);
---
+++ JavaScript/swapping.js
@@ -0,0 +1,13 @@
+var num1=10,num2=20;
+// var num3;
+// var num3=num1;
+// var num1=num2;
+// var num2=num3;
+// console.log(num1,num2);
+
+num1=num1+num2;
+console.log(num1);
+num2=num1-num2;
+console.log(num2);
+num1=num1-num2;
+console.log(num1);
